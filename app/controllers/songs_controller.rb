35535d26c125e1e9595b3760de75c9ccea78f037
Dave <davecharnuska@gmail.com>
2014-10-30 03:04:12 -0400

    add edit, update, show actions and tag_params, handle_tags methods to Songs Controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,8 +8,11 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
+    @tags = tag_params
+    
     @song.user_id = current_user.id
     if @song.save
+      handle_tags
       redirect_to user_url(current_user.id)
     else
       flash.now[:errors] = @song.errors.full_messages
@@ -18,10 +21,42 @@ class SongsController < ApplicationController
   end
 
   def edit
+    @song = Song.find(params[:id])
+    render :edit
+  end
+  
+  def update
+    @song = Song.find(params[:id])
+    @tags = tag_params
+    
+    if @song.update(song_params)
+      handle_tags
+      redirect_to song_url(@song)
+    else
+      flash.now[:errors] = @song.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @song = Song.find(params[:id])
   end
 
   private
   def song_params
     params.require(:song).permit(:title, :release_date, :about, :credits, :privacy, :band_id, :album_id)
+  end
+  
+  def tag_params
+    params.require(:tag).permit(:names)
+  end
+  
+  def handle_tags
+    tags = tag_params[:names].split(",")
+    tags.each do |tag|
+      @tag = Tag.find_or_create_by(name: tag)
+      @type = @song.class.name
+      Tagging.find_or_create_by(tag_id: @tag.id, taggable_id: @song.id, taggable_type: @type)
+    end
   end
 end
