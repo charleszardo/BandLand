07058500b60473552e186a6da0e039a0db72518f
Dave <davecharnuska@gmail.com>
2014-10-30 03:00:46 -0400

    add edit, update, show actions and tag_params, handle_tags methods to Bands Controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -8,8 +8,11 @@ class BandsController < ApplicationController
 
   def create
     @band = Band.new(band_params)
+    @tags = tag_params
+    
     @band.user_id = current_user.id
     if @band.save
+      handle_tags
       redirect_to user_url(current_user.id)
     else
       flash.now[:errors] = @band.errors.full_messages
@@ -18,10 +21,42 @@ class BandsController < ApplicationController
   end
 
   def edit
+    @band = Band.find(params[:id])
+    render :edit
+  end
+  
+  def update
+    @band = Band.find(params[:id])
+    @tags = tag_params
+    
+    if @band.update(band_params)
+      handle_tags
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
   end
 
   private
   def band_params
     params.require(:band).permit(:name, :location, :website, :genre, :info)
+  end
+  
+  def tag_params
+    params.require(:tag).permit(:names)
+  end
+  
+  def handle_tags
+    tags = tag_params[:names].split(",")
+    tags.each do |tag|
+      @tag = Tag.find_or_create_by(name: tag)
+      @type = @band.class.name
+      Tagging.find_or_create_by(tag_id: @tag.id, taggable_id: @band.id, taggable_type: @type)
+    end
   end
 end
--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -2,11 +2,7 @@ class TaggingsController < ApplicationController
 
   def destroy
     @tagging = Tagging.find(params[:id].to_i)
-    # tagged_type = @tagging.taggable_type
-    # tagged_id = @tagging.taggable_id
-    # redirect_url = tagged_type
     @tagging.destroy
-    # redirect_to bands_url
     redirect_to request.referer
   end
   
