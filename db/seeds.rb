8feed8c4043c96cd9d59680b7f33fab0ebfebc43
Dave <davecharnuska@gmail.com>
2014-10-30 21:40:09 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,34 @@
 Album.create!([
-  {name: "Future Days", release_date: "1971-10-30", about: "", credits: "", privacy: "public", band_id: 8, user_id: 16},
-  {name: "Double Fantasy", release_date: nil, about: "", credits: "", privacy: "public", band_id: 9, user_id: 17}
+  {name: "Future Days", release_date: "1971-10-30", about: "", credits: "", privacy: "public", band_id: 1, user_id: 1},
+  {name: "Double Fantasy", release_date: nil, about: "", credits: "", privacy: "public", band_id: 2, user_id: 2},
+  {name: "Tago Mago", release_date: "1971-02-01", about: "", credits: "", privacy: "public", band_id: 1, user_id: 1},
+  {name: "Live Peace in Toronto 1969", release_date: "1969-12-12", about: "", credits: "", privacy: "public", band_id: 2, user_id: 2},
+  {name: "Monster Movie", release_date: "1969-08-01", about: "", credits: "", privacy: "public", band_id: 1, user_id: 1}
 ])
 Band.create!([
-  {name: "Can", location: "Berlin", website: "", genre: "Experimental", info: "", user_id: 16},
-  {name: "Plastic Ono Band", location: "", website: "", genre: "Rock", info: "", user_id: 17}
+  {name: "Can", location: "Berlin", website: "", genre: "Experimental", info: "", user_id: 1},
+  {name: "Plastic Ono Band", location: "", website: "", genre: "Rock", info: "", user_id: 2}
 ])
 Song.create!([
-  {title: "Future Days", release_date: nil, about: "", credits: "", privacy: "public", album_id: 11, band_id: 8, user_id: 16}
+  {title: "Future Days", release_date: nil, about: "", credits: "", privacy: "public", album_id: 1, band_id: 1, user_id: 1}
+  {title: "Spray", release_date: nil, about: "", credits: "", privacy: "public", album_id: 1, band_id: 1, user_id: 1}
+  {title: "Moonshake", release_date: nil, about: "", credits: "", privacy: "public", album_id: 1, band_id: 1, user_id: 1}
+  {title: "Bel Air", release_date: nil, about: "", credits: "", privacy: "public", album_id: 1, band_id: 1, user_id: 1}
 ])
 Tag.create!([
   {name: "cool"},
-  {name: " kraut"},
-  {name: "luv"}
+  {name: "so kraut"},
+  {name: "psychedelic as phuk"},
+  {name: "heady"},
+  {name: "das best"}
 ])
 Tagging.create!([
-  {tag_id: 31, taggable_id: 8, taggable_type: "Band"},
-  {tag_id: 32, taggable_id: 8, taggable_type: "Band"},
-  {tag_id: 33, taggable_id: 9, taggable_type: "Band"}
+  {tag_id: 1, taggable_id: 1, taggable_type: "Band"},
+  {tag_id: 1, taggable_id: 1, taggable_type: "Album"},
+  {tag_id: 2, taggable_id: 3, taggable_type: "Album"},
+  {tag_id: 3, taggable_id: 1, taggable_type: "Album"},
+  {tag_id: 4, taggable_id: 2, taggable_type: "Band"},
+  {tag_id: 5, taggable_id: 2, taggable_type: "Album"},
 ])
 User.create!([
   {username: "holger", password_digest: "$2a$10$BxgIoIP5e0hvykw1KqcnWuQIsuChLTlt782MVv1VEUdlH5vavgkBC", session_token: "7iYBbrY0VngT1LfD-xtGCQ", location: nil, bio: nil, email: "holger@can.com"},
