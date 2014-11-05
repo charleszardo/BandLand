e418cc4a16351bbb41992c26e5abeadcbc34f732
Dave <davecharnuska@gmail.com>
2014-11-05 12:09:53 -0500

    add open view to edit song on show page

--- app/assets/javascripts/models/song.js
+++ app/assets/javascripts/models/song.js
@@ -1,3 +1,13 @@
 BandLand.Models.Song = Backbone.Model.extend({
-	urlRoot: "/api/songs"
+	urlRoot: "/api/songs",
+	
+	toJSON: function () {
+		var json = Backbone.Model.prototype.toJSON.call(this);
+	
+		delete json.id;
+		delete json.created_at;
+		delete json.updated_at;
+	
+		return json;
+	}
 });
--- app/assets/javascripts/views/songs/show.js
+++ app/assets/javascripts/views/songs/show.js
@@ -1,16 +1,39 @@
 BandLand.Views.SongsShow = Backbone.View.extend({
-	template: JST["songs/show"],
+	template: function () {
+		return this.open ? JST["songs/edit"] : JST["songs/show"];
+	},
 	
 	events: {
-		"click button.destroy": "destroy"
+		"click button.destroy": "destroy",
+		"dblclick div.title": "beginEditing",
+		"submit form": "endEditing"
 	},
 	
 	initialize: function (options) {
 		this.listenTo(this.model, "sync", this.render);
+		this.open = false;
+	},
+	
+	beginEditing: function () {
+		this.open = true;
+		this.render();
+	},
+	
+	endEditing: function (event) {
+		event.preventDefault();
+		this.open = false;
+		
+		var params = $(event.currentTarget).serializeJSON();
+		this.model.set(params["title"]);
+		this.model.save();
+		// var title = this.$("textarea.song_title").val();
+		// this.model.save({ title: title });
+		
+		this.render();
 	},
 	
 	render: function () {
-		var renderedContent = this.template({
+		var renderedContent = this.template()({
 			song: this.model
 		});
 		
